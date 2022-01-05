1a046b27ed7f455ec769c0cae376421b2ab7c095
StackZombie <khuramrazamw@gmail.com>
2022-01-06 04:54:17 +0500

    Updating source code

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,10 +6,10 @@ app.use(bodyParser.json());
 
 app.post("/events", (req, res) => {
   const events = req.body;
-  axios.post("http://localhost:4000/events", events);
-  axios.post("http://localhost:4001/events", events);
-  axios.post("http://localhost:4002/events", events);
-  axios.post("http://localhost:4003/events", events);
+  axios.post("http://post-clusterip-srv/events", events);
+  // axios.post("http://localhost:4001/events", events);
+  // axios.post("http://localhost:4002/events", events);
+  // axios.post("http://localhost:4003/events", events);
 
   res.send({ status: "OK" });
 });
