c01810d5d303cd9b05d9a03e5825d328970847c3
StackZombie <khuramrazamw@gmail.com>
2022-01-10 06:20:21 +0500

    Working on infra

--- Moderation/index.js
+++ Moderation/index.js
@@ -12,7 +12,7 @@ app.post("/events", async (req, res) => {
     const { id, comment, postId } = data;
     const status = comment.includes("orange") ? "rejected" : "approved";
     try {
-      await axios.post("http://localhost:4005/events", {
+      await axios.post("http://event-bus-srv:4005/events", {
         type: "CommentModerated",
         data: {
           id,
